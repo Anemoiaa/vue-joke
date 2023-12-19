0fb8412feece78bd5a0b56f79a41a4abb8671fde
anemoiaa <georgiypetroff@gmail.com>
2023-12-19 20:19:40 +0400

    refactor:made a joke as a component

---
+++ src/components/JokeCard.vue
@@ -0,0 +1,24 @@
+<template>
+	<div
+			class="flex items-center gap-4 py-4"
+	>
+		<img
+				class="w-10 h-10"
+				src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" alt="">
+		<p class="text-base">{{ props.joke.value }}</p>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	joke: {
+		required: true,
+		type: Object,
+	}
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
